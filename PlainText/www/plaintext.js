498250dccc031908fd005c35b0631601df6f1753
Tom Gersic <tgersic@modelmetrics.com>
2012-08-03 00:20:25 -0500

    refactor page transition

--- PlainText/www/plaintext.js
+++ PlainText/www/plaintext.js
@@ -22,56 +22,63 @@ function init() {
         var url = $.mobile.path.parseUrl(data.toPage);
         var editurl = /^#edit/;
         if (url.hash.search(editurl) !== -1) {
-            //url scheme is a bit mucked up, so get rid of the initial "edit?" after the hash so we can parse it like a normal url
-            var paramstring = url.hash.replace(/^#edit\?/,"#");
-            //get the id, username, and password from the query string
-            var id = $.url(paramstring).fparam('Id');
-            var username = $.url(paramstring).fparam('Username__c');
-            var password = $.url(paramstring).fparam('Password__c');
-            var name = $.url(paramstring).fparam('Name');
-            var siteUrl = $.url(paramstring).fparam('URL__c');
-
-            var pageSelector = url.hash.replace(/\?.*$/, "");
-            if(id) {
-                //select the page
-                var page = $(pageSelector);
-                //put the site name in the header
-                var header = page.find('#title');
-                header.html(name);
-                //select the content element within it
-                var content = page.children(":jqmData(role=content)");
-                //add the URL
-                var urlField = content.find('#siteUrl');
-                urlField.html("<a href='"+siteUrl+"' target='_blank'>"+siteUrl+"</a>");
-
-                //fill out the fields
-                var idField = content.find('#id');
-                idField.val(id);
-                var usernameField = content.find('#username');
-                usernameField.val(username);
-                var passwordField = content.find('#password');
-                passwordField.val(password);
-
-                //monitor for changes to fields
-                usernameField.change(function(){
-                    console.log('username changed');
-                    fieldsChanged=true;
-                });
-
-                passwordField.change(function(){
-                    console.log('password changed');
-                    fieldsChanged=true;
-                });
-
-                //we're intercepting the page change event, so change the page
-                $.mobile.changePage(page);
-
-                //stop the default page change actions from occurring
-                event.preventDefault();
-            }                  
+            changeToEditPage(url,event);
         }
     }
   });
+}
+
+/**
+ * Change to Edit Page
+ **/
+function changeToEditPage(url,event) {
+    //url scheme is a bit mucked up, so get rid of the initial "edit?" after the hash so we can parse it like a normal url
+    var paramstring = url.hash.replace(/^#edit\?/,"#");
+    //get the id, username, and password from the query string
+    var id = $.url(paramstring).fparam('Id');
+    var username = $.url(paramstring).fparam('Username__c');
+    var password = $.url(paramstring).fparam('Password__c');
+    var name = $.url(paramstring).fparam('Name');
+    var siteUrl = $.url(paramstring).fparam('URL__c');
+
+    var pageSelector = url.hash.replace(/\?.*$/, "");
+    if(id) {
+        //select the page
+        var page = $(pageSelector);
+        //put the site name in the header
+        var header = page.find('#title');
+        header.html(name);
+        //select the content element within it
+        var content = page.children(":jqmData(role=content)");
+        //add the URL
+        var urlField = content.find('#siteUrl');
+        urlField.html("<a href='"+siteUrl+"' target='_blank'>"+siteUrl+"</a>");
+
+        //fill out the fields
+        var idField = content.find('#id');
+        idField.val(id);
+        var usernameField = content.find('#username');
+        usernameField.val(username);
+        var passwordField = content.find('#password');
+        passwordField.val(password);
+
+        //monitor for changes to fields
+        usernameField.change(function(){
+            console.log('username changed');
+            fieldsChanged=true;
+        });
+
+        passwordField.change(function(){
+            console.log('password changed');
+            fieldsChanged=true;
+        });
+
+        //we're intercepting the page change event, so change the page
+        $.mobile.changePage(page);
+
+        //stop the default page change actions from occurring
+        event.preventDefault();
+    }                  
 }
 
 /**
