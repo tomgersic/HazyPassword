8d39f2e51c2f353e1aa28d465e2fd7f106215a9e
Tom Gersic <tgersic@modelmetrics.com>
2012-08-03 11:42:50 -0500

    made checkConnection() more self-sufficient

--- PlainText/www/plaintext.js
+++ PlainText/www/plaintext.js
@@ -106,7 +106,7 @@ function loadRecords(error) {
  **/
 function loadRecordsFromSalesforce(soupExists){
   //check if we're online
-  if(checkConnection() && !FAKE_OFFLINE){
+  if(checkConnection()){
     console.log('We are online... querying SFDC');
     forcetkClient.query("SELECT Id, Name, Username__c, Password__c, URL__c FROM Password__c", function(response){
     //console.log(response);
@@ -213,23 +213,27 @@ function storeRecords(records,error){
  * return true if we're online
  **/
 function checkConnection() {
-    var networkState = navigator.network.connection.type;
+  if(FAKE_OFFLINE){
+    return false;
+  }
+  
+  var networkState = navigator.network.connection.type;
 
-    var states = {};
-    states[Connection.UNKNOWN]  = 'Unknown connection';
-    states[Connection.ETHERNET] = 'Ethernet connection';
-    states[Connection.WIFI]     = 'WiFi connection';
-    states[Connection.CELL_2G]  = 'Cell 2G connection';
-    states[Connection.CELL_3G]  = 'Cell 3G connection';
-    states[Connection.CELL_4G]  = 'Cell 4G connection';
-    states[Connection.NONE]     = 'No network connection';
+  var states = {};
+  states[Connection.UNKNOWN]  = 'Unknown connection';
+  states[Connection.ETHERNET] = 'Ethernet connection';
+  states[Connection.WIFI]     = 'WiFi connection';
+  states[Connection.CELL_2G]  = 'Cell 2G connection';
+  states[Connection.CELL_3G]  = 'Cell 3G connection';
+  states[Connection.CELL_4G]  = 'Cell 4G connection';
+  states[Connection.NONE]     = 'No network connection';
 
-    if(states[networkState] == states[Connection.NONE] || states[networkState] == states[Connection.UNKNOWN]){
-      return false;
-    }
-    else {
-      return true;
-    }
+  if(states[networkState] == states[Connection.NONE] || states[networkState] == states[Connection.UNKNOWN]){
+    return false;
+  }
+  else {
+    return true;
+  }
 }
 
 /**
